2ae60394fafb98fc5304e81d9e8137578d9ca243
Gabriel Sutherland <gabriel.2sutherland@gmail.com>
2022-06-14 13:37:25 -0700

    commit

--- src/HTMLscript.js
+++ src/HTMLscript.js
@@ -56,11 +56,11 @@ function newElement() {
 
   //Creating Cookies for website storage
   //function setCookie() {
-    var TaskName =document.getElementById("myInput");
+    
     
 
-  document.cookie = "TaskID= please; expires= wed, 27 Jul 2022 12:00:00 UTC; path=/";
-
+document.cookie = "TaskID= please; expires= wed, 27 Jul 2022 12:00:00 UTC; path=/";
+document.cookie = "name=test";
   //}
 
 }
